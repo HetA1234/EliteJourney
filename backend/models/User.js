c208042efa2a132d442acf5d9cd6ec84b12be366
Heta <115974373+HetA1234@users.noreply.github.com>
2024-10-31 21:15:44 -0400

    Delete backend/models directory

--- backend/models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-	uid: String,
-	email: String,
-	displayName: String,
-});
-const User = mongoose.model('User', userSchema);
-export default User;
